e35f16f952683e37dfe6b7d17869a48516aa6ff7
Nishant Raj <nravztar7@gmail.com>
2025-07-13 12:56:19 +0530

    Update SelectionSort.java

--- sorting/src/SelectionSort.java
+++ sorting/src/SelectionSort.java
@@ -1,2 +1,25 @@
-public class SelectionSort {
-}
+ public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        selectionSort(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i =0; i<n;i++){
+            int minIndex=i;
+            for (int j=i+1;j<n;j++){
+                if (arr[minIndex]>arr[j]){
+                    minIndex=j;
+
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex]=arr[i];
+            arr[i]=temp;
+        }
+    }
